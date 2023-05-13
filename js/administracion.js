d4db103cde3cf457a91b6d8d55f874d8cffd2964
Mauricio Soria <mauricioutn2017@gmail.com>
2023-05-13 01:43:19 -0300

    Version 2.0

--- js/administracion.js
+++ js/administracion.js
@@ -13,7 +13,7 @@ let id = document.getElementById(`id`),
     tiempo = document.getElementById(`tiempo`),
     categoria = document.getElementById(`categoria`),
     descripcion = document.getElementById(`descripcion`);
-
+    
 let altaServicio = true;
 let listaServicios = JSON.parse(localStorage.getItem("listaServicios")) || [];
 
@@ -51,18 +51,17 @@ function crearFila(servicio, fila) {
     ${servicio.imagen}
     </td>
     <td>${servicio.profesor}</td>
-    <td class="text-truncate truncateWidth">${servicio.descripcion}</td>
     <td>${servicio.categoria}</td>
     <td><i class="bi bi-currency-dollar"></i>${servicio.precio}</td>
     <td><i class="bi bi-clock"></i>${servicio.tiempo}</td>
     <td class="">
-    <button class="btn btn-warning text-dark"><i class="bi bi-pencil-square"></i></button>
-    <button class="btn btn-danger"><i class="bi bi-x-square"></i></button>
+    <button class="btn btn-warning text-dark"onclick="prepararFormularioServicio()"><i class="bi bi-pencil-square"></i></button>
+    <button class="btn btn-danger" onclick="borrarServicio()"><i class="bi bi-x-square"></i></button>
     </td>
 </tr>`;
 }
 // Manejadores de eventos
-formServicio.addEventListener("submit", prepararFormularioPelicula);
+formServicio.addEventListener("submit", prepararFormularioServicio);
 btnCrearServicio.addEventListener("click", desplegarmodalServicio);
 
 // Funciones
@@ -72,17 +71,18 @@ function desplegarmodalServicio() {
     modalServicio.show();
 }
 
-function prepararFormularioPelicula(e) {
+function prepararFormularioServicio(e) {
     e.preventDefault();
-    altaServicio=true
     console.log("en el evento submit");
     if (altaServicio) {
         crearServicio();
     } else {
-        
+        editarServicio();
     }
 }
 function crearServicio() {
+    //AQUI VAN LAS VALIDACIONES
+
     // Crear servicio
     console.log(`hola`);
     const servicioNuevo = new Servicio(
@@ -115,9 +115,101 @@ function crearServicio() {
 }
 
 function guardarEnLocalstorage() {
-    localStorage.setItem("listaServicios", JSON.stringify(listaServicios));
+    localStorage.setItem("listaServicios",JSON.stringify(listaServicios));
 }
 
 function limpiarFormulario() {
     formServicio.reset();
+}
+
+window.borrarServicio = (id) => {
+    Swal.fire({
+        title: "¿Esta seguro de borrar este servicio?",
+        text: "No podrás recuperar la informacion luego de eliminar",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Borrar",
+        cancelButtonText: "Cancelar",
+    }).then((result) => {
+        console.log(result);
+        if (result.isConfirmed) {
+            //id cuando borro
+            console.log(id);
+            //1 - buscar del array a donde esta el elemento que tiene este id
+            let listaServicios = listaServicios.findIndex((servicio) => servicio.id === id);
+            //2 - borrar el servicio del array (splice)
+            listaServicios.splice(posicionServicio, 1);
+            //3 - actualizar el localstorage
+            guardarEnLocalstorage();
+            //4- borrar la fila de la tabla
+            let tablaServicio = document.getElementById("tablaServicio");
+            tablaServicio.removeChild(tablaServicio.children[posicionServicio]);
+            //5 - mostrar un cartel al usuario
+            Swal.fire(
+                "Servicio eliminada",
+                "El servicio seleccionado fue eliminado correctamente",
+                "success"
+            );
+            //todo: paso 6 actualizar los numeros de las filas de la tabla.
+        }
+    });
+};
+
+window.prepararFormularioServicio = (id) => {
+    //tener los datos de la servicio y cargar en el formulario
+    const servicioEncontrado = listaServicios.find(
+        (servicio) => servicio.id === id
+    );
+    //mostrar la ventana modal
+    id.value = servicioEncontrado.id;
+    servicioNombre.value = servicioEncontrado.servicioNombre;
+    profesor.value = servicioEncontrado.profesor;
+    socialProf.value = servicioEncontrado.socialProf;
+    imagen.value = servicioEncontrado.imagen;
+    tiempo.value = servicioEncontrado.tiempo;
+    precio.value = servicioEncontrado.precio;
+    descripcion.value = servicioEncontrado.descripcion;
+    categoria.value = servicioEncontrado.categoria;
+    modalServicio.show();
+    //cambiamos el valor de la variable altaServicio
+    altaServicio = false;
+};
+
+function editarServicio(){
+    console.log("aqui tengo que editar");
+    //1- buscaria la posicion de la servicio en el array
+    let posicionServicio = listaServicios.findIndex((servicio) => servicio.id === id.value);
+    console.log(posicionServicio)
+    //todo: validar los datos
+    //2- editar los valores de la servicio dentroe del array
+    listaServicios[posicionServicio].servicioNombre = servicioNombre.value;
+    listaServicios[posicionServicio].imagen = imagen.value;
+    listaServicios[posicionServicio].descripcion = descripcion.value;
+    listaServicios[posicionServicio].categoria = categoria.value;
+    listaServicios[posicionServicio].profesor = profesor.value;
+    listaServicios[posicionServicio].socialProf = socialProf.value;
+    listaServicios[posicionServicio].tiempo = tiempo.value;
+    listaServicios[posicionServicio].precio = precio.value;
+
+    //3- actualizar el localstorage
+    guardarEnLocalstorage()
+    //4-actualizar la fila
+    let tablaServicio = document.getElementById("tablaServicio");
+    console.log(tablaServicio.children[posicionServicio].children[1])
+    //  let celdaTitulo =tablaServicio.children[posicionServicio].children[1]
+    tablaServicio.children[posicionServicio].children[1].innerHTML = titulo.value
+    tablaServicio.children[posicionServicio].children[2].children[0].innerHTML = descripcion.value
+    tablaServicio.children[posicionServicio].children[3].children[0].innerHTML = imagen.value
+    tablaServicio.children[posicionServicio].children[4].innerHTML = precio.value
+    //5-mostrar un cartel al usuario
+    Swal.fire(
+        "Servicio modificada",
+        "El Servicio fue modificado exitosamente",
+        "success"
+    );
+    //6- limpiar el formulario y cerrar el modal
+    limpiarFormulario();
+    modalServicio.hide();
 }
