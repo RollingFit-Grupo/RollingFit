7280fdc2f13f5eeaf13840d43fa840b874f80cd8
Luciano Angeleri <lcnang@gmail.com>
2023-05-13 20:59:58 -0300

    Se definieron las variables para obtener los textos y se creó una funcion para elegir una palabra aleatoria del array de palabras

--- js/animaciones.js
+++ js/animaciones.js
@@ -30,3 +30,24 @@ function fadeIn(entradas, mostrarEnScroll) {
 seccionesFade.forEach(seccion => mostrarEnScroll.observe(seccion))
 seccionesTranslateIzq.forEach(seccion => mostrarEnScroll.observe(seccion))
 seccionesTranslateDer.forEach(seccion => mostrarEnScroll.observe(seccion))
+
+/*Animaciones de Titulo*/
+let TextoGrupo1 = document.getElementById("TextoGrupo1")
+let TextoGrupo2 = document.getElementById("TextoGrupo2")
+const palabrasGrupo1 = ['Transformá', 'Cambiá', 'Elevá'];
+const palabrasGrupo2 = ['tu cuerpo', 'tu mente', 'tu vida'];
+
+let posicionGrupo1 = 0;
+let posicionGrupo2 = 0;
+
+let palabraGrupo1Actual = TextoGrupo1.textContent = palabrasGrupo1[posicionGrupo1]
+let palabraGrupo2Actual = TextoGrupo2.textContent = palabrasGrupo2[posicionGrupo2]
+
+function generarPosicionRandom(posicionAnterior, numPosiciones) {
+    let nuevaPosicion = Math.floor(Math.random() * numPosiciones);
+    while (nuevaPosicion === posicionAnterior) {
+        nuevaPosicion = Math.floor(Math.random() * numPosiciones);
+    }
+    return nuevaPosicion;
+}
+
