c3fa9415ceca7540dc28aeeb6f59944cf44b3ee9
rita <ritaastorga10@gmail.com>
2023-05-14 17:47:58 -0300

    agregué comentario para implementar libreria uuidv4 para validar el campo ID

--- js/helpers.js
+++ js/helpers.js
@@ -1,5 +1,7 @@
 // helpers para validar los campos del formulario de Admin
 
+/* Para validar el campo ID recordar colocar en el constructor un parámetro por defecto, en este caso será la direccion de la librería uuidv4 que dejaré en la card de helpers del panel de trello. Así mismo se deberá linkear dicha libreria en el archivo Admin.html */
+
 function validarCantidadCaracteres(texto, min, max) {
   if (texto.legth >= min && texto.legth <= max) {
     console.log("la palabra es válida");
