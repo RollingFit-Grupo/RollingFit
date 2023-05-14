49e7c7004c36f1831bf8638b2dcc138070d1e2d2
Julieta Correa <mjcorrea210@gmail.com>
2023-05-14 20:53:16 -0300

    mejoras en el codigo y nuevo archivo utils

--- js/app.js
+++ js/app.js
@@ -1,32 +1,57 @@
+import {
+  getLocalStorage,
+  setLocalStorage,
+  hideElement,
+  showElement,
+  closeModal,
+} from "./utils.js";
+
+// Forms
+
 const loginForm = document.getElementById("form-login");
 const registerForm = document.getElementById("form-register");
 
-const loginButton = document.getElementById("login-button");
-const registerButton = document.getElementById("register-button");
-
-const showLoginButton = document.getElementById("show-login-button");
-const showRegisterButton = document.getElementById("show-register-button");
-
-const modalTitle = document.getElementById("auth-modal-title");
-
-const registerInputName = document.getElementById("register-name");
-const registerInputEmail = document.getElementById("register-email");
-const registerInputPassword = document.getElementById("register-password");
+// Inputs
 
 const loginInputEmail = document.getElementById("login-email");
 const loginInputPassword = document.getElementById("login-password");
 
+// Buttons
+
+const loginButton = document.getElementById("login-button");
+const registerButton = document.getElementById("register-button");
+const initSessionButton = document.getElementById("init-session-button");
+const closeSessionButton = document.getElementById("close-session-button");
+const showLoginButton = document.getElementById("show-login-button");
+const showRegisterButton = document.getElementById("show-register-button");
+
+// Others
+
+const modalTitle = document.getElementById("auth-modal-title");
 const userType = document.getElementById("user-type");
 
+// Class AdminUser
+
+class AdminUser {
+  constructor(name, email, password) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.isAdmin = true;
+  }
+}
+
+// Initial functions
+
 registerAdminUser();
+checkUserLogged();
 
-function hideElement(element) {
-  element.classList.add("d-none");
-}
+// Events listeners
 
-function showElement(element) {
-  element.classList.remove("d-none");
-}
+loginButton.addEventListener("click", loginUser);
+closeSessionButton.addEventListener("click", closeSession);
+showRegisterButton.addEventListener("click", showRegisterForm);
+showLoginButton.addEventListener("click", showLoginForm);
 
 function changeModalTitle(title) {
   modalTitle.innerHTML = title;
@@ -59,65 +84,90 @@ function showLoginForm() {
 }
 
 function registerAdminUser() {
-  // Creamos un objeto que es un usuario administrador
-  const adminUser = {
-    name: "Julieta Correa",
-    email: "mjulieta210@gmail.com",
-    password: "12345",
-    isAdmin: true,
-  };
-
+  // Creamos un objeto que es un usuario administrador instanciando la clase AdminUser
+  const adminUser = new AdminUser(
+    "Julieta Correa",
+    "mjulieta210@gmail.com",
+    "12345"
+  );
   // guardamos el usuario en localStorage
-  localStorage.setItem("users", JSON.stringify([adminUser]));
+  setLocalStorage("users", [adminUser]);
 }
 
 //Ingreso como usario//
 
 function loginUser() {
-  if (loginInputEmail.value && loginInputPassword.value) {
-    const raw = localStorage.getItem("users");
-    const localData = JSON.parse(raw);
+  const email = loginInputEmail.value;
+  const password = loginInputPassword.value;
 
-    const userFound = localData.find(
-      (user) => user.email === loginInputEmail.value
+  if (!email || !password) {
+    Swal.fire(
+      "Completa todos los campos",
+      "Debes completar todos los campos para iniciar sesion",
+      "info"
+    );
+    return;
+  }
+
+  const raw = getLocalStorage("users");
+  const users = JSON.parse(raw);
+  const userFound = users.find((user) => user.email === email);
+
+  if (!userFound) {
+    Swal.fire(
+      "Credenciales incorrectas",
+      "Usuario o contraseña incorrectas",
+      "error"
     );
 
-    if (userFound) {
-      if (userFound.password === loginInputPassword.value) {
-        if (userFound.isAdmin === true) {
-          showElement(userType);
-          userType.innerHTML = "Administrador";
+    return;
+  }
 
-          closeModal("login-modal");
-        }
-      } else {
-        alert("Usuario o password incorrecto");
-      }
-    } else {
-      alert("Usuario o password incorrecto");
-    }
-  } else {
-    alert("Por favor ingresa un usuario valido!");
+  if (userFound.password !== password) {
+    Swal.fire(
+      "Credenciales incorrectas",
+      "Usuario o contraseña incorrectas",
+      "error"
+    );
+
+    return;
+  }
+
+  if (userFound.isAdmin === true) {
+    showElement(userType);
+    userType.innerHTML = "Administrador";
+    sessionStorage.setItem("user-session", JSON.stringify(userFound));
+    closeModal("login-modal");
+    hideElement(initSessionButton);
+    showElement(closeSessionButton);
+
+    Swal.fire(
+      "Inicio de sesion exitoso",
+      "Te logueaste exitosamente como administrador",
+      "success"
+    );
   }
 }
 
 // Retorna una instancia del modal
 
-function closeModal(modalId) {
-  let myModalEl = document.getElementById(modalId);
-  let myModal = bootstrap.Modal.getInstance(myModalEl);
+function checkUserLogged() {
+  const raw = sessionStorage.getItem("user-session");
+  const userLogged = JSON.parse(raw);
 
-  myModal.hide();
-
-  myModalEl.classList.remove("show");
-  document.body.classList.remove("modal-open");
-
-  // Elimina el elemento del backdrop del DOM
-  let backdrop = document.querySelector(".modal-backdrop");
-  if (backdrop) {
-    backdrop.parentNode.removeChild(backdrop);
-
-    // Elimina el estilo de overflow
-    document.body.style.overflow = "";
+  if (userLogged) {
+    showElement(userType);
+    userType.innerHTML = "Administrador";
+    hideElement(initSessionButton);
+    showElement(closeSessionButton);
+  } else {
+    hideElement(userType);
+    hideElement(closeSessionButton);
+    showElement(initSessionButton);
   }
+}
+
+function closeSession() {
+  sessionStorage.removeItem("user-session");
+  checkUserLogged();
 }
