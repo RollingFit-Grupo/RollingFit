a3e3a487c42366a979a1d531f8f0aec3a95f1936
Luciano Angeleri <lcnang@gmail.com>
2023-05-13 21:30:12 -0300

    Se agregaron funciones para cambiar el texto de manera aleatoria y para cambiar la clase con el efecto correspondiente

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,4 +1,4 @@
-/*Animaciones de Scroll*/
+/*Variables de Animaciones de Scroll*/
 const seccionesFade = document.querySelectorAll(".fade-in");
 const seccionesTranslateIzq = document.querySelectorAll(".fade-izquierda");
 const seccionesTranslateDer = document.querySelectorAll(".fade-derecha");
@@ -14,7 +14,19 @@ const opcionesTranslate = {
 };
 
 const mostrarEnScroll = new IntersectionObserver(fadeIn, opcionesFade);
+/*Variables de Animaciones de Texto*/
+let TextoGrupo1 = document.getElementById("TextoGrupo1")
+let TextoGrupo2 = document.getElementById("TextoGrupo2")
+const palabrasGrupo1 = ['Transformá', 'Cambiá', 'Elevá'];
+const palabrasGrupo2 = ['tu cuerpo', 'tu mente', 'tu vida'];
 
+let posicionGrupo1 = 0;
+let posicionGrupo2 = 0;
+
+let palabraGrupo1Actual = TextoGrupo1.textContent = palabrasGrupo1[posicionGrupo1]
+let palabraGrupo2Actual = TextoGrupo2.textContent = palabrasGrupo2[posicionGrupo2]
+
+/*Funciones de Animaciones de Scroll*/
 function fadeIn(entradas, mostrarEnScroll) {
     entradas.forEach(entrada => {
         if (!entrada.isIntersecting) {
@@ -31,18 +43,7 @@ seccionesFade.forEach(seccion => mostrarEnScroll.observe(seccion))
 seccionesTranslateIzq.forEach(seccion => mostrarEnScroll.observe(seccion))
 seccionesTranslateDer.forEach(seccion => mostrarEnScroll.observe(seccion))
 
-/*Animaciones de Titulo*/
-let TextoGrupo1 = document.getElementById("TextoGrupo1")
-let TextoGrupo2 = document.getElementById("TextoGrupo2")
-const palabrasGrupo1 = ['Transformá', 'Cambiá', 'Elevá'];
-const palabrasGrupo2 = ['tu cuerpo', 'tu mente', 'tu vida'];
-
-let posicionGrupo1 = 0;
-let posicionGrupo2 = 0;
-
-let palabraGrupo1Actual = TextoGrupo1.textContent = palabrasGrupo1[posicionGrupo1]
-let palabraGrupo2Actual = TextoGrupo2.textContent = palabrasGrupo2[posicionGrupo2]
-
+/*Funciones de Animaciones de Texto*/
 function generarPosicionRandom(posicionAnterior, numPosiciones) {
     let nuevaPosicion = Math.floor(Math.random() * numPosiciones);
     while (nuevaPosicion === posicionAnterior) {
@@ -50,4 +51,24 @@ function generarPosicionRandom(posicionAnterior, numPosiciones) {
     }
     return nuevaPosicion;
 }
-
+function cambiarTexto() {
+    let nuevaPosicionGrupo1 = generarPosicionRandom(posicionGrupo1,palabrasGrupo1.length);
+    posicionGrupo1 = nuevaPosicionGrupo1;
+    palabraGrupo1Actual = TextoGrupo1.textContent = palabrasGrupo1[posicionGrupo1];    
+    
+    let nuevaPosicionGrupo2 =  generarPosicionRandom(posicionGrupo2,palabrasGrupo2.length);
+    posicionGrupo2 = nuevaPosicionGrupo2;
+    palabraGrupo2Actual = TextoGrupo2.textContent = palabrasGrupo2[posicionGrupo2];
+}
+function cambiarClaseTexto() {
+    if (TextoGrupo1.classList.contains("slide-texto-fromBottom")) {
+        TextoGrupo1.classList.replace("slide-texto-fromBottom","slide-texto-toTop")
+    }else if(TextoGrupo1.classList.contains("slide-texto-toTop")){        
+        TextoGrupo1.classList.replace("slide-texto-toTop","slide-texto-fromBottom")
+    }
+    if (TextoGrupo2.classList.contains("slide-texto-fromTop")) {
+        TextoGrupo2.classList.replace("slide-texto-fromTop","slide-texto-toBottom")
+    }else if(TextoGrupo2.classList.contains("slide-texto-toBottom")){        
+        TextoGrupo2.classList.replace("slide-texto-toBottom","slide-texto-fromTop")
+    }
+}
