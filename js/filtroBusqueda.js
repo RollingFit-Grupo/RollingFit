9670ad0ca41c8221bf975730465c7b342efe64d4
Luciano Angeleri <lcnang@gmail.com>
2023-05-17 22:23:56 -0300

    Se agregó la funcionalidad para mostrar y ocultar el mensaje de advertencia

--- js/filtroBusqueda.js
+++ js/filtroBusqueda.js
@@ -1,35 +1,43 @@
 let inputBusqueda = document.getElementById("input_filtro_Servicios");
 console.log(inputBusqueda.value)
-let mensajeAdvertencia = `
-<div class="alert alert-warning rounded-5 d-flex flex-column justify-content-center">
-    <i class="bi bi-search fs-1 p-3 align-self-center"></i>
-    <h5 class="display-5 p-4"><strong>No se encontraron</strong> Servicios...</h5>
-</div>`
+let mensajeAdvertencia = document.getElementById("mensajeServicioNoEncontrado")
 
 function busquedaServicio(nombreServicio) {
     let seccionServicio = document.getElementById("seccionServicios");
     let cardsServicio = seccionServicio.querySelectorAll(".card");
+    let nombreServicioBuscado = nombreServicio.toLowerCase();
     let resultadosEncontrados = false;
     cardsServicio.forEach(card => {
         let tituloCard = card.querySelector("h4")
         let tituloServicio = tituloCard.textContent.toLowerCase() 
-        console.log(card)
-        console.log(tituloServicio)
         if (nombreServicio === "") {
-            card.classList.remove("d-none"); // Muestra la tarjeta
-            let resultadosEncontrados = true;
-        } else if (tituloServicio.includes(nombreServicio)) {
-            card.classList.add("d-block"); // Muestra la tarjeta
-            card.classList.remove("d-none"); // Asegura que no esté oculta
-            let resultadosEncontrados = true;
+            card.classList.remove("d-none");
+            resultadosEncontrados = true;
+        } else if (tituloServicio.includes(nombreServicioBuscado)) {
+            card.classList.add("d-block"); 
+            card.classList.remove("d-none");
+            resultadosEncontrados = true;
         } else {
-            card.classList.remove("d-block"); // Oculta la tarjeta
-            card.classList.add("d-none"); // Asegura que esté oculta
+            card.classList.remove("d-block");
+            card.classList.add("d-none");
+            resultadosEncontrados = false;
         }
     });
-    console.log(resultadosEncontrados)
-
+    if (resultadosEncontrados) {
+        ocultarMensaje();
+    } else {
+        mostrarMensaje();
+    }
 }
+function mostrarMensaje() {
+    mensajeAdvertencia.classList.remove("d-none");
+    mensajeAdvertencia.classList.add("d-flex");
+  }
+  
+  function ocultarMensaje() {
+    mensajeAdvertencia.classList.remove("d-flex");
+    mensajeAdvertencia.classList.add("d-none");
+  }
 
 
 inputBusqueda.addEventListener("input", ()=>{
